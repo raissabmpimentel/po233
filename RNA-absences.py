d5f02b904ee2210e698b53e0619394adf27f6a7b
Gianluigi Dal Toso <gianluigi.toso@gmail.com>
2020-06-02 17:09:41 -0300

    Change absences Score to SME

--- RNA-absences.py
+++ RNA-absences.py
@@ -5,10 +5,17 @@ import pandas as pd
 import numpy as np
 
 def RNA_Regression(dataV, targetV, seed, algorithm, max_iteractions, es):
-    data_train, data_test, target_train, target_test = train_test_split(dataV, targetV, random_state=seed, test_size=0.2)
+    # Regression
+    data_train, data_test, target_train, target_test = train_test_split(dataV, targetV, random_state=seed, test_size=0.1)
     regression = MLPRegressor(random_state=seed, early_stopping=es, max_iter=max_iteractions, solver=algorithm).fit(data_train, target_train)
+    
+    # Regression Score
     score = regression.score(data_test, target_test)
-    return score
+    
+    # Square Mean Error
+    target_pred = regression.predict(data_test)
+    sqe = mean_squared_error(target_test, target_pred)
+    return (sqe, score)
 
 # Import data
 data_filename = "dados/pre_processed_data.csv"
@@ -22,48 +29,49 @@ original = data
 # Scikit Learn MLP Regression
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 # My training
 seed = 1
-algorithm = 'sgd'
+algorithm = 'adam' # 'adam', 'sgd', 'lbfgs'
 max_iteractions = 10000
 early_stopping = True
 
 # {G1, G2, G3}
-score = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
-print(f"[G1, G2, G3] - Score: {score}")
+(sqe, score) = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
+print(f"[G1, G2, G3] - SQE: {sqe:.5f}    |    Score: {score:.5f}")
 
 # {G1, G2}
 data = original.drop(columns=['G3'])
-score = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
-print(f"[G1, G2]     - Score: {score}")
+(sqe, score) = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
+print(f"[G1, G2]     - SQE: {sqe:.5f}    |    Score: {score:.5f}")
 
 # {G1, G3}
 data = original.drop(columns=['G2'])
-score = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
-print(f"[G1, G3]     - Score: {score}")
+(sqe, score) = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
+print(f"[G1, G3]     - SQE: {sqe:.5f}    |    Score: {score:.5f}")
 
 # {G2, G3}
 data = original.drop(columns=['G1'])
-score = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
-print(f"[G2, G3]     - Score: {score}")
+(sqe, score) = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
+print(f"[G2, G3]     - SQE: {sqe:.5f}    |    Score: {score:.5f}")
 
 # {G1}
 data = original.drop(columns=['G2', 'G3'])
-score = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
-print(f"[G1]         - Score: {score}")
+(sqe, score) = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
+print(f"[G1]         - SQE: {sqe:.5f}    |    Score: {score:.5f}")
 
 # {G2}
 data = original.drop(columns=['G1', 'G3'])
-score = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
-print(f"[G2]         - Score: {score}")
+(sqe, score) = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
+print(f"[G2]         - SQE: {sqe:.5f}    |    Score: {score:.5f}")
 
 # {G3}
 data = original.drop(columns=['G1', 'G2'])
-score = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
-print(f"[G3]         - Score: {score}")
+(sqe, score) = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
+print(f"[G3]         - SQE: {sqe:.5f}    |    Score: {score:.5f}")
 
 # {}
 data = original.drop(columns=['G1', 'G2', 'G3'])
-score = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
-print(f"[]           - Score: {score}")
+(sqe, score) = RNA_Regression(data, target, seed, algorithm, max_iteractions, early_stopping)
+print(f"[]           - SQE: {sqe:.5f}    |    Score: {score:.5f}")
