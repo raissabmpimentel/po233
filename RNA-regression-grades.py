5963aac2ada06acd3289c3d7a28d15e9d0cb8888
Raissa Pimentel <raissabmpimentel@gmail.com>
2020-06-17 00:27:20 -0300

    New regression

--- RNA-regression-grades.py
+++ RNA-regression-grades.py
@@ -5,9 +5,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from math import sqrt
+from sklearn.preprocessing import StandardScaler
 
 def treinamento(test_size, max_iter, solver, activation, random_state, verbose, tol):
     previsores_treinamento, previsores_teste, y_treinamento, y_teste = train_test_split(previsores, y, test_size=test_size, random_state=0)
+    
+    prep = StandardScaler()
+            
+    prep.fit(previsores_treinamento)
     
     regressor = MLPRegressor(verbose = verbose,
                                   max_iter=max_iter,
@@ -17,15 +22,15 @@ def treinamento(test_size, max_iter, solver, activation, random_state, verbose, 
                                   random_state=random_state,
                                   )
     
-    regressor.fit(previsores_treinamento, y_treinamento)
-    previsoes = regressor.predict(previsores_teste)
+    regressor.fit(prep.transform(previsores_treinamento), y_treinamento)
+    previsoes = regressor.predict(prep.transform(previsores_teste))
     
-    mse = sqrt(mean_squared_error(y_teste, previsoes))
-    score = regressor.score(previsores_teste, y_teste)
+    mse = mean_squared_error(y_teste, previsoes)
+    score = regressor.score(prep.transform(previsores_teste), y_teste)
     return (mse, score)
 
-base = pd.read_csv('dados/pre_processed_data.csv')
-y = base['G3'].values
+#base = pd.read_csv('dados/pre_processed_data.csv')
+#y = base['G3'].values
 
 test_size = 0.2
 max_iter = 1500
@@ -33,14 +38,26 @@ solver = 'adam'
 activation = 'relu'
 random_state = 4
 verbose = False
-tol = 1e-10
+tol = 1e-5
 
+base = pd.read_csv('dados/student-por.csv', sep = ";")
+base = base.drop(['school'], axis=1)
+base = base.replace(['LE3', 'GT3'], [0,1])
+items = ['sex', 'address','Pstatus','Mjob','Fjob','reason','guardian','schoolsup','famsup','paid','activities','nursery','higher',
+         'internet','romantic']
+
+for item in items:
+    base = pd.concat([base,pd.get_dummies(base[item], prefix=item)],axis=1)
+    base = base.drop([item],axis=1)
+
+y = base['G3'].values
 
 # [G1, G2]
 
 print(f"[G1, G2]:\n")
 previsores = base.drop(columns=['G3']).values
 precisao_1, score_1 = treinamento(test_size, max_iter, solver, activation, random_state, verbose, tol)
+
 print(f"MSE: {precisao_1} Score: {score_1}\n")
 
 # [G1]
